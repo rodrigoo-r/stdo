9662a5116f02c525cd9838cb4680df950ea8fb3d
rodrigoo-r <2nw53j32643y@gmail.com>
2025-04-29 13:06:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# stdo
+
+stdo, short for standard output, is a mini-extension of
+the stdio C library.
+
+## License
+
+This project is licensed under the GNU GPL-3.0 License. See the [LICENSE](LICENSE) file for details.
---
+++ library.h
@@ -0,0 +1,16 @@
+/*
+* This code is distributed under the terms of the GNU General Public License.
+ * For more information, please refer to the LICENSE file in the root directory.
+ * -------------------------------------------------
+ * Copyright (C) 2025 Rodrigo R.
+ * This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions; type `show c' for details.
+*/
+
+#ifndef STDO_LIBRARY_H
+#define STDO_LIBRARY_H
+
+void hello(void);
+
+#endif //STDO_LIBRARY_H
